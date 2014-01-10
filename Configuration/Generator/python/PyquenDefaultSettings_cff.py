b196465c4c043020c89d8d9a574de0c7a9413e53
Yetkin <yetkin.yilmaz@cern.ch>
2014-01-11 00:51:38 +0100

    removd my wrong drum settings

--- Configuration/Generator/python/PyquenDefaultSettings_cff.py
+++ Configuration/Generator/python/PyquenDefaultSettings_cff.py
@@ -14,74 +14,34 @@ collisionParameters2760GeV = cms.PSet(aBeamTarget = cms.double(208.0), ## beam/t
 
 collisionParameters = collisionParameters2760GeV.clone()
 
+qgpParameters = cms.PSet(qgpInitialTemperature = cms.double(1.0), ## initial temperature of QGP; allowed range [0.2,2.0]GeV;
+                         qgpProperTimeFormation = cms.double(0.1), ## proper time of QGP formation; allowed range [0.01,10.0]fm/c;
+                         hadronFreezoutTemperature = cms.double(0.14),
+                         doRadiativeEnLoss = cms.bool(True), ## if true, perform partonic radiative en loss
+                         doCollisionalEnLoss = cms.bool(False),
+                         qgpNumQuarkFlavor = cms.int32(0),  ## number of active quark flavors in qgp; allowed values: 0,1,2,3 
+                         numQuarkFlavor = cms.int32(0) ## to be removed
+                         )
+
 pyquenParameters  = cms.PSet(doIsospin = cms.bool(True),
                              angularSpectrumSelector = cms.int32(0), ## angular emitted gluon spectrum :
                              embeddingMode = cms.bool(False),
                              backgroundLabel = cms.InputTag("generator") ## ineffective in no mixing
                              )
 
-hydjetCommonParameters = cms.PSet(collisionParameters,
-                                  rotateEventPlane = cms.bool(True),
-                                  allowEmptyEvents = cms.bool(False),
-                                  embeddingMode = cms.bool(False),
-                                  qgpInitialTemperature = cms.double(1.0), ## initial temperature of QGP; allowed range [0.2,2.0]GeV;
-                                  qgpProperTimeFormation = cms.double(0.1), ## proper time of QGP formation; allowed range [0.01,10.0]fm/c;
-                                  qgpNumQuarkFlavor = cms.int32(0),  ## number of active quark flavors in qgp; allowed values: 0,1,2,3
-                                  fracSoftMultiplicity = cms.double(1.),                                  
-                                  )
-
-hydjetBassParameters = cms.PSet(hydjetCommonParameters,
-                                sigmaInelNN = cms.double(58),
-                                shadowingSwitch = cms.int32(0),
-                                nMultiplicity = cms.int32(21500),
-                                maxLongitudinalRapidity = cms.double(4.5),
-                                maxTransverseRapidity = cms.double(1.),
-                                hadronFreezoutTemperature = cms.double(0.14),
-                                doQuench = cms.bool(True),
-                                doCollisionalEnLoss = cms.bool(False),
-                                doRadiativeEnLoss = cms.bool(True), ## if true, perform partonic radiative en loss
-                                )
-
-
-hydjetDrumParameters = cms.PSet(hydjetCommonParameters,
-                                sigmaInelNN = cms.double(64),
-                                shadowingSwitch = cms.int32(1),
-                                nMultiplicity = cms.int32(18500),
-                                maxLongitudinalRapidity = cms.double(4.5),
-                                maxTransverseRapidity = cms.double(1.15),
-                                hadronFreezoutTemperature = cms.double(0.125),
-                                doQuench = cms.bool(True),                                
-                                doCollisionalEnLoss = cms.bool(True),
-                                doRadiativeEnLoss = cms.bool(True), ## if true, perform partonic radiative en loss
-                                )
-
-
+hydjetParameters = cms.PSet(sigmaInelNN = cms.double(58),
+                            shadowingSwitch = cms.int32(0),
+                            nMultiplicity = cms.int32(21500),
+                            fracSoftMultiplicity = cms.double(1.),
+                            maxLongitudinalRapidity = cms.double(4.5),
+                            maxTransverseRapidity = cms.double(1.),
+                            rotateEventPlane = cms.bool(True),
+                            allowEmptyEvents = cms.bool(False),
+                            embeddingMode = cms.bool(False)                            
+                            )
 
 pyquenPythiaDefaultBlock = cms.PSet(
     pythiaUESettingsBlock,
-    pythiaUESettingsDrum = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function',
-                                       'MSTJ(22)=2     ! Decay those unstable particles',
-                                       'PARJ(71)=10 .  ! for which ctau  10 mm',
-                                       'MSTP(2)=1      ! which order running alphaS',
-                                       'MSTP(33)=0     ! no K factors in hard cross sections',
-                                       'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)',
-                                       'MSTP(52)=2     ! work with LHAPDF',
-                                       'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default',
-                                       'MSTP(82)=4     ! Defines the multi-parton model',
-                                       'MSTU(21)=1     ! Check on possible errors during program execution',
-                                       'PARP(82)=1.8387   ! pt cutoff for multiparton interactions',
-                                       'PARP(89)=1960. ! sqrts for which PARP82 is set',
-                                       'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter',
-                                       'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter',
-                                       'PARP(90)=0.16  ! Multiple interactions: rescaling power',
-                                       'PARP(67)=2.5    ! amount of initial-state radiation',
-                                       'PARP(85)=1.0  ! gluon prod. mechanism in MI',
-                                       'PARP(86)=1.0  ! gluon prod. mechanism in MI',
-                                       'PARP(62)=1.25   ! ',
-                                       'PARP(64)=0.2    ! ',
-                                       'MSTP(91)=1      !',
-                                       'PARP(91)=2.1   ! kt distribution',
-                                       'PARP(93)=15.0  ! '),    
     hydjetPythiaDefault = cms.vstring('MSEL=0   ! user processes',
                                       'CKIN(3)=6.',# ! ptMin
                                       'MSTP(81)=0'
@@ -279,8 +239,8 @@ pyquenPythiaDefaultBlock = cms.PSet(
     'PARJ(71)=40.'
     ),
     myParameters = cms.vstring(
-    ),
-    decayParameters = cms.vstring('MDCY(310,1)=0'),        
+    )
+    
 )    
 
 # This one is not to be used
