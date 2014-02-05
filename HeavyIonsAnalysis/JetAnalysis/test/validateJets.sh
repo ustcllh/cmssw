a1f550d4df3615a8f9ad0f64a107e631451ad13d
Yetkin <yetkin.yilmaz@cern.ch>
2014-02-05 22:47:13 +0100

    reco cfgs and macro

--- HeavyIonsAnalysis/JetAnalysis/test/validateJets.sh
+++ HeavyIonsAnalysis/JetAnalysis/test/validateJets.sh
@@ -3,6 +3,7 @@
 scp lxplus.cern.ch:/afs/cern.ch/user/a/alcaprod/public/Alca/GlobalTag/STARTHI53_LV1.db .
 
 cmsRun runReco_PbPb_MC_53X.py
+cmsRun runForest_PbPb_MIX_53X.py
 
 root -l quickResponse.C
 
