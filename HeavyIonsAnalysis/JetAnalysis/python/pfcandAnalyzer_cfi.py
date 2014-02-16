9831c90be5ae46eb11174f1898a087142360459d
Yen-Jie <yenjie@mit.edu>
2014-02-16 14:10:12 -0500

    Added Voronoi related python configuration to pfcandAnalyzer_cfi.py

--- HeavyIonsAnalysis/JetAnalysis/python/pfcandAnalyzer_cfi.py
+++ HeavyIonsAnalysis/JetAnalysis/python/pfcandAnalyzer_cfi.py
@@ -10,5 +10,8 @@ pfcandAnalyzer = cms.EDAnalyzer('HiPFCandAnalyzer',
                                 jetPtMin = cms.double(20.0),                                
                                 verbosity = cms.untracked.int32(0),
                                 skipCharged = cms.untracked.bool(False)
+                                doJets_ = cms.bool(False),
+                                doVS = cms.untracked.bool(True),
+                                bkg = cms.InputTag("voronoiBackgroundPF"),
                                 )
 
