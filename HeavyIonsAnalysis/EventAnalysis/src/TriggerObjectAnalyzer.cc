9b6ecd2954c5c4a66c76d0b0f7151b460714bce7
Kurt <kurtejung@gmail.com>
2014-03-24 15:06:02 -0400

    addition of all jet trigger objects to tree

--- HeavyIonsAnalysis/EventAnalysis/src/TriggerObjectAnalyzer.cc
+++ HeavyIonsAnalysis/EventAnalysis/src/TriggerObjectAnalyzer.cc
@@ -65,6 +65,7 @@ class TriggerObjectAnalyzer : public edm::EDAnalyzer {
 
   edm::Service<TFileService> fs;
   vector<TNtuple*> nt_;
+  int verbose_;
 };
 
 //
@@ -86,8 +87,8 @@ TriggerObjectAnalyzer::TriggerObjectAnalyzer(const edm::ParameterSet& ps):
 {
    //now do what ever initialization is needed
   nt_.reserve(triggerNames_.size());
-  nt_[0] = fs->make<TNtuple>(triggerNames_[0].data(),triggerNames_[0].data(),"id:pt:eta:phi:mass");
-
+  nt_[0] = fs->make<TNtuple>("jetObjTree","HLT_HIJet*_Triggers","id:pt:eta:phi:mass");
+  verbose_ = 0;
 }
 
 
@@ -116,42 +117,46 @@ TriggerObjectAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& 
    iEvent.getByLabel(triggerEventTag_,triggerEventHandle_);
    iEvent.getByLabel(triggerResultsTag_,triggerResultsHandle_);
 
-   //moduleIndex_ = triggerResultsHandle_->index(triggerIndex_);
-   const unsigned int mIndex = triggerResultsHandle_->index(triggerIndex_);
+   for(unsigned int itrig=0; itrig<triggerNames_.size(); itrig++){
+    
+       triggerIndex_ = hltConfig_.triggerIndex(triggerNames_[itrig]);
+       
+       const unsigned int mIndex = triggerResultsHandle_->index(triggerIndex_);
 
-   // Results from TriggerEvent product - Attention: must look only for
-   // modules actually run in this path for this event!
-   for (unsigned int j=0; j<=mIndex; ++j) {
-     // check whether the module is packed up in TriggerEvent product
-     string trigFilterIndex = hltConfig_.moduleLabels(triggerNames_[0]).at(j); //this is simple to put into a loop to get all triggers...
-     const unsigned int filterIndex(triggerEventHandle_->filterIndex(InputTag(trigFilterIndex,"",processName_)));
-     if (filterIndex<triggerEventHandle_->sizeFilters()) {
-       const trigger::Vids& VIDS (triggerEventHandle_->filterIds(filterIndex));
-       const trigger::Keys& KEYS(triggerEventHandle_->filterKeys(filterIndex));
-       const unsigned int nI(VIDS.size());
-       const unsigned int nK(KEYS.size());
-       assert(nI==nK);
-       const unsigned int n(max(nI,nK));
+       // Results from TriggerEvent product - Attention: must look only for
+       // modules actually run in this path for this event!
+       for (unsigned int j=0; j<=mIndex; ++j) {
+           // check whether the module is packed up in TriggerEvent product
+           string trigFilterIndex = hltConfig_.moduleLabels(triggerIndex_).at(j); //this is simple to put into a loop to get all triggers...
+           const unsigned int filterIndex(triggerEventHandle_->filterIndex(InputTag(trigFilterIndex,"",processName_)));
+           if (filterIndex<triggerEventHandle_->sizeFilters()) {
+               const trigger::Vids& VIDS (triggerEventHandle_->filterIds(filterIndex));
+               const trigger::Keys& KEYS(triggerEventHandle_->filterKeys(filterIndex));
+               const unsigned int nI(VIDS.size());
+               const unsigned int nK(KEYS.size());
+               assert(nI==nK);
+               const unsigned int n(max(nI,nK));
 
-       const trigger::TriggerObjectCollection& TOC(triggerEventHandle_->getObjects());
-       for (unsigned int i=0; i!=n; ++i) {
-           const trigger::TriggerObject& TO(TOC[KEYS[i]]);
-           //This check prevents grabbing the L1 trigger object (VIDS < 0), and finds the max trigger pt within all trigger collections
-           if(VIDS[i]>0 && pt<TO.pt()){
-               //cout << "   " << i << " " << VIDS[i] << "/" << KEYS[i] << ": "
-               //              << TO.id() << " " << TO.pt() << " " << TO.et() << " " << TO.eta() << " " << TO.phi() << " " << TO.mass()
-               //                        << endl;
-               id = TO.id();
-               pt = TO.pt();
-               eta = TO.eta();
-               phi = TO.phi();
-               mass = TO.mass();
+               const trigger::TriggerObjectCollection& TOC(triggerEventHandle_->getObjects());
+               for (unsigned int i=0; i!=n; ++i) {
+                   const trigger::TriggerObject& TO(TOC[KEYS[i]]);
+                   //This check prevents grabbing the L1 trigger object (VIDS < 0), and finds the max trigger pt within all trigger collections
+                   if(VIDS[i]>0 && pt<TO.pt()){
+                       //cout << "   " << i << " " << VIDS[i] << "/" << KEYS[i] << ": "
+                       //              << TO.id() << " " << TO.pt() << " " << TO.et() << " " << TO.eta() << " " << TO.phi() << " " << TO.mass()
+                       //                        << endl;
+                       id = TO.id();
+                       pt = TO.pt();
+                       eta = TO.eta();
+                       phi = TO.phi();
+                       mass = TO.mass();
+                   }
+               }
            }
        }
-     }
    }
 
-   nt_[0]->Fill(id,pt,eta,phi,mass);
+    nt_[0]->Fill(id,pt,eta,phi,mass);
   }
 }
 
@@ -175,28 +180,29 @@ TriggerObjectAnalyzer::beginRun(edm::Run const& iRun, edm::EventSetup const& iSe
   bool changed(true);
   if (hltConfig_.init(iRun,iSetup,processName_,changed)) {
     if (changed) {
-      if (triggerNames_[0]!="@") { // "@" means: analyze all triggers in config
-	const unsigned int n(hltConfig_.size());
-	if(n > 0){
-	  triggerIndex_ = hltConfig_.triggerIndex(triggerNames_[0]);
-	  moduleLabels_ = hltConfig_.moduleLabels(triggerIndex_);
-	}
+        const unsigned int n(hltConfig_.size());
+        for(unsigned int itrig=0; itrig<triggerNames_.size(); itrig++){
+            if (triggerNames_[itrig]!="@") { // "@" means: analyze all triggers in config
 
-	if (triggerIndex_>=n) {
-	  cout << "HLTEventAnalyzerAOD::analyze:"
-	       << " TriggerName " << triggerNames_[0] 
-	       << " not available in (new) config!" << endl;
-	  cout << "Available TriggerNames are: " << endl;
-	  hltConfig_.dump("Triggers");
-	}
-      }
-      hltConfig_.dump("ProcessName");
-      hltConfig_.dump("GlobalTag");
-      hltConfig_.dump("TableName");
-      hltConfig_.dump("Streams");
-      hltConfig_.dump("Datasets");
-      hltConfig_.dump("PrescaleTable");
-      hltConfig_.dump("ProcessPSet");
+                //This functionality is currently not working for me (kjung - mar 24, 2014)... no trigger passes the condition
+                if (triggerIndex_>=n) {
+                    //cout << "HLTEventAnalyzerAOD::analyze:"
+                    //    << " TriggerName " << triggerNames_[0] 
+                    //    << " not available in (new) config!" << endl;
+                    //cout << "Available TriggerNames are: " << endl;
+                    //hltConfig_.dump("Triggers");
+                }
+            }
+        }
+        if(verbose_){
+            hltConfig_.dump("ProcessName");
+            hltConfig_.dump("GlobalTag");
+            hltConfig_.dump("TableName");
+            hltConfig_.dump("Streams");
+            hltConfig_.dump("Datasets");
+            hltConfig_.dump("PrescaleTable");
+            hltConfig_.dump("ProcessPSet");
+        }
     }
   } else {
     cout << "HLTEventAnalyzerAOD::analyze:"
