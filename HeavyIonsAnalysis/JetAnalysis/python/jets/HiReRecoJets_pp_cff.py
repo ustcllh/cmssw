f9a0806649626ef2000980462cf5a9b9ebdd5516
Doga <dgulhan@cern.ch>
2015-10-29 10:52:37 +0100

    PF jets were not defined

--- HeavyIonsAnalysis/JetAnalysis/python/jets/HiReRecoJets_pp_cff.py
+++ HeavyIonsAnalysis/JetAnalysis/python/jets/HiReRecoJets_pp_cff.py
@@ -1,6 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 from RecoHI.HiJetAlgos.HiRecoJets_cff import *
 from RecoHI.HiJetAlgos.HiRecoPFJets_cff import *
+
+ak1PFJets = ak5PFJets.clone(rParam       = cms.double(0.1))
+ak2PFJets = ak5PFJets.clone(rParam       = cms.double(0.2))
+ak3PFJets = ak5PFJets.clone(rParam       = cms.double(0.3))
+ak4PFJets = ak5PFJets.clone(rParam       = cms.double(0.4))
+ak6PFJets = ak5PFJets.clone(rParam       = cms.double(0.6))
+ak7PFJets = ak5PFJets.clone(rParam       = cms.double(0.7))
+
+ak1PFJets = ak5PFJets.clone 
 akVs1PFJets.jetPtMin = 1
 akVs1PFJets.src = cms.InputTag("particleFlow")
 akVs1CaloJets.jetPtMin = 1
