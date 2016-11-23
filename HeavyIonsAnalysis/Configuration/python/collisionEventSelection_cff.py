34358148022b909f9c81ed28bd30521c081504a6
Yetkin <yetkin.yilmaz@cern.ch>
2016-11-23 11:28:26 +0100

    event selection updates for both pA and AA

--- HeavyIonsAnalysis/Configuration/python/collisionEventSelection_cff.py
+++ HeavyIonsAnalysis/Configuration/python/collisionEventSelection_cff.py
@@ -10,10 +10,20 @@ primaryVertexFilter = cms.EDFilter("VertexSelector",
     filter = cms.bool(True),   # otherwise it won't filter the events
 )
 
+primaryVertexFilterPA = cms.EDFilter("VertexSelector",
+    src = cms.InputTag("offlinePrimaryVertices"),
+    cut = cms.string("!isFake && abs(z) <= 25 && position.Rho <= 2 && tracksSize >= 2"),
+    filter = cms.bool(True), # otherwise it won't filter the events
+)
+
+
 # Cluster-shape filter re-run offline
 from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
 from HLTrigger.special.hltPixelClusterShapeFilter_cfi import *
 hltPixelClusterShapeFilter.inputTag = "siPixelRecHits"
+
+# Cluster-shape filter re-run offline from ClusterCompatibility object
+from HeavyIonsAnalysis.EventAnalysis.HIClusterCompatibilityFilter_cfi import *
 
 # Reject BSC beam halo L1 technical bits
 from L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff import *
@@ -23,8 +33,24 @@ noBSChalo = hltLevel1GTSeed.clone(
     L1SeedsLogicalExpression = cms.string('NOT (36 OR 37 OR 38 OR 39)')
 )
 
-collisionEventSelection = cms.Sequence(noBSChalo *
-                                       hfCoincFilter3 *
+#Reject beam scraping events standard pp configuration
+NoScraping = cms.EDFilter("FilterOutScraping",
+ applyfilter = cms.untracked.bool(True),
+ debugOn = cms.untracked.bool(False),
+ numtrack = cms.untracked.uint32(10),
+ thresh = cms.untracked.double(0.25)
+)
+
+collisionEventSelection = cms.Sequence(hfCoincFilter3 *
                                        primaryVertexFilter *
-                                       siPixelRecHits *
-                                       hltPixelClusterShapeFilter)
+                                       clusterCompatibilityFilter)
+
+collisionEventSelection_HaloAndMonster = cms.Sequence(noBSChalo *
+                                                      siPixelRecHits *
+                                                      hltPixelClusterShapeFilter)
+
+collisionEventSelectionPA = cms.Sequence(hfCoincFilter *
+                                         primaryVertexFilterPA *
+                                         NoScraping
+                                         )
+
