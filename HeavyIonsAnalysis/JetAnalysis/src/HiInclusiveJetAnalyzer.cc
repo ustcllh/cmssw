6b902afc14adac2eeaabc41d3c6abacd0ce546e3
Chris <cfmcginn@mit.edu>
2018-02-16 12:20:16 +0100

    Fix to constituent phi in hiinclusivejetanalyzer

--- HeavyIonsAnalysis/JetAnalysis/src/HiInclusiveJetAnalyzer.cc
+++ HeavyIonsAnalysis/JetAnalysis/src/HiInclusiveJetAnalyzer.cc
@@ -2875,7 +2875,7 @@ void HiInclusiveJetAnalyzer::fillNewJetVarsRecoJet(const reco::Jet jet){
       jcid.push_back(dp.pdgId());
       jcE.push_back(jc.e());
       jcpt.push_back(jc.pt());
-      jcphi.push_back(jc.phi());
+      jcphi.push_back(jc.phi_std());
       jceta.push_back(jc.eta());
       jcm.push_back(jc.m());
       
@@ -3364,7 +3364,7 @@ void HiInclusiveJetAnalyzer::fillNewJetVarsRefJet(const reco::GenJet jet){
       jcid.push_back(dp.pdgId());
       jcE.push_back(jc.e());
       jcpt.push_back(jc.pt());
-      jcphi.push_back(jc.phi());
+      jcphi.push_back(jc.phi_std());
       jceta.push_back(jc.eta());
       jcm.push_back(jc.m());
 
@@ -3852,7 +3852,7 @@ void HiInclusiveJetAnalyzer::fillNewJetVarsGenJet(const reco::GenJet jet){
       jcid.push_back(dp.pdgId());
       jcE.push_back(jc.e());
       jcpt.push_back(jc.pt());
-      jcphi.push_back(jc.phi());
+      jcphi.push_back(jc.phi_std());
       jceta.push_back(jc.eta());
       jcm.push_back(jc.m());
 
